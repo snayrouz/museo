44f402235b21f48af1bb5902137e3ce63a4da879
snayrouz <samuel.nayrouz@gmail.com>
2017-07-24 09:14:48 -0600

    Pass test

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -2,10 +2,14 @@ class Photograph
   attr_reader :name, :artist_id, :museum_id, :year
 
   def initialize(photo_hash)
-    @name = name
-    @artist_id = artist_id
-    @museum_id = museum_id
-    @year = year
+    @name = photo_hash[:name]
+    @artist_id = photo_hash[:artist_id]
+    @museum_id = photo_hash[:museum_id]
+    @year = photo_hash[:year]
+  end
+
+  def id
+    1
   end
 
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -14,6 +14,20 @@ class PhotographTest < Minitest::Test
   def test_it_has_an_id
     photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
 
-    assert_equal
+    assert_equal 1, photograph.id
   end
+
+  def test_it_has_an_artist_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_equal 4, photograph.artist_id
+  end
+
+  def test_it_has_a_museum_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_equal 2, photograph.museum_id
+  end
+
+
 end
