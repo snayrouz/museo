ff83df80fe542ee56cd2fee4615ea7eceee1bd61
snayrouz <samuel.nayrouz@gmail.com>
2017-07-24 09:09:29 -0600

    Initial commit

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,2 +1,11 @@
 class Photograph
+  attr_reader :name, :artist_id, :museum_id, :year
+
+  def initialize(photo_hash)
+    @name = name
+    @artist_id = artist_id
+    @museum_id = museum_id
+    @year = year
+  end
+
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -4,4 +4,16 @@ require 'minitest/pride'
 require './lib/photograph'
 
 class PhotographTest < Minitest::Test
+
+  def test_it_exist
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_instance_of Photograph, photograph
+  end
+
+  def test_it_has_an_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_equal
+  end
 end
