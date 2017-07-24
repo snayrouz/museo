a99272eaea80ff82ae0237955249d364fb897ea2
snayrouz <samuel.nayrouz@gmail.com>
2017-07-24 09:17:29 -0600

    Complete iteration 1

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -29,5 +29,16 @@ class PhotographTest < Minitest::Test
     assert_equal 2, photograph.museum_id
   end
 
+  def test_it_has_a_name
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", photograph.name
+  end
+
+  def test_it_has_a_year
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+
+    assert_equal 1954, photograph.year
+  end
 
 end
